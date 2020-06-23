6f24c6caf274908de1673991b71eb0c45b87ab56
Hudson <hmpace@asu.edu>
2020-06-22 21:30:46 -0700

    Allow users to change their text color.

--- public/core.js
+++ public/core.js
@@ -3,8 +3,27 @@ var socket = io();
 chatApp.controller("mainController", function($scope) {
     var messageCounter = 0;
     $scope.messages = [];
+    $scope.textStyle = {};
     $scope.send = function() {
-        socket.emit('chat message', $scope.message);
+        if ($scope.message[0] === '/') {
+            command = $scope.message.split(' ');
+            switch (command[0]) {
+                case '/changecolor':
+                    var s = new Option().style;
+                    s.color = command[1]
+                    if (s.color === command[1]) {
+                        $scope.textStyle.color = command[1];
+                    }
+                    break;
+            }
+        }
+        else {
+            msg = {
+                text: $scope.message,
+                style: $scope.textStyle
+            }
+            socket.emit('chat message', msg);
+        }
         $scope.message = "";
     };
     socket.on('chat message', function(msg) {
@@ -13,7 +32,8 @@ chatApp.controller("mainController", function($scope) {
             $scope.messages.push(
                 {
                     index: messageCounter,
-                    message: msg
+                    message: msg.text,
+                    style: msg.style
                 }
             );
         });
