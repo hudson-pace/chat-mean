3b525e3f8ec253c70fc29eeb966ece4edf408e2b
Hudson <hudsonmpace@gmail.com>
2020-07-04 10:30:49 -0700

    Add models for users and refresh tokens.

---
+++ users/refresh-token.model.js
@@ -0,0 +1,35 @@
+/* defines schema for refresh tokens collection in database */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
+    token: String,
+    expires: Date,
+    created: {type: Date, default: Date.now},
+    createdByIp: String,
+    revoked: Date,
+    revokedByIp: String,
+    replacedByToken: String
+});
+
+schema.virtual('isExpired').get(function() {
+    return Date.now() >= this.expires;
+});
+
+schema.virtual('isActive').get(function() {
+    return !this.revoked && !this.isExpired;
+});
+
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function(doc, ret) {
+        delete ret._id;
+        delete ret.id;
+        delete ret.user;
+    }
+});
+
+module.exports = mongoose.model('RefreshToken', schema);
---
+++ users/user.model.js
@@ -0,0 +1,21 @@
+/* defines schema for users collection in database */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    username: {type: String, unique: true, required: true},
+    passwordHash: {type: String, required: true},
+    role: {type: String, required: true}
+});
+
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function(doc, ret) {
+        delete ret._id;
+        delete ret.passwordHash;
+    }
+});
+
+module.exports = mongoose.model('User', schema);
