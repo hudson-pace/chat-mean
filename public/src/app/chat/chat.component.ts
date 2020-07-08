65fd2b55ef202e42f39d1430d4a395f7f7c508ee
Hudson <hudsonmpace@gmail.com>
2020-07-08 08:42:21 -0700

    unsubscribe from observables when chat is closed.

--- public/src/app/chat/chat.component.ts
+++ public/src/app/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ChatService } from '../services/chat.service'
 import { AuthenticationService } from '../services/authentication.service'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -9,24 +10,28 @@ import { AuthenticationService } from '../services/authentication.service'
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  private subscription: Subscription = new Subscription();
   messages = [];
   username = '';
   message = '';
   constructor(private chatService: ChatService, private authenticationService: AuthenticationService) {
-    this.authenticationService.user.subscribe(x => {
+    this.subscription.add(this.authenticationService.user.subscribe(x => {
       if (x) {
         this.username = x.username;
       }
-    });
+    }));
 
-    this.chatService.receiveMessages().subscribe(x => this.messages.push(x));
+    this.subscription.add(this.chatService.receiveMessages().subscribe(x => this.messages.push(x)));
   }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   send() {
-    console.log("here we go: " + this.message);
     this.chatService.sendMessage({ id: 0, text: this.message, from: this.username });
     this.message = '';
   }
