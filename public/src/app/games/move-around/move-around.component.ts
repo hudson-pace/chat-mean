c8086ed247353b532111c004ac65342459f7dc33
Hudson <hudsonmpace@gmail.com>
2020-07-11 23:14:56 -0700

    add canvas for game to be drawn on

--- public/src/app/games/move-around/move-around.component.ts
+++ public/src/app/games/move-around/move-around.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { CONTEXT_NAME } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-move-around',
   templateUrl: './move-around.component.html',
   styleUrls: ['./move-around.component.css']
 })
-export class MoveAroundComponent implements OnInit {
+export class MoveAroundComponent implements OnInit, AfterViewInit {
+  @ViewChild('gameCanvas', {static: false}) gameCanvas: ElementRef;
+  context: CanvasRenderingContext2D;
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.context = this.gameCanvas.nativeElement.getContext('2d');
+    this.context.moveTo(0, 0);
+    this.context.lineTo(300, 200);
+    this.context.stroke();
   }
 
 }
