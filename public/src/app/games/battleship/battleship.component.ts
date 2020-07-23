b4d4534f1fc1779e6716aaa8203140cb714d7b5f
Hudson <hudsonmpace@gmail.com>
2020-07-23 03:34:48 -0700

    Allow users to place their boats

--- public/src/app/games/battleship/battleship.component.ts
+++ public/src/app/games/battleship/battleship.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵSWITCH_IVY_ENABLED__POST_R3__ } from '@angular/core';
 
 enum Phase {
     Start,
     Setup,
     MainGame,
+}
+type Square = {
+  hasBoat: boolean;
+  isHit: boolean;
+  isSelected: boolean;
+  isValid: boolean;
 }
 
 @Component({
@@ -13,11 +19,14 @@ enum Phase {
 })
 export class BattleshipComponent implements OnInit {
   Phase = Phase;
-  playerBoard: number[][][];
-  enemyBoard: number[][][];
+  playerBoard: Square[][];
+  enemyBoard: Square[][];
   height: number = 8;
   width: number = 8;
   phase: Phase = Phase.Start;
+  ships: number[] = [4, 3, 2];
+  currentShip: number = this.ships.pop();
+  orientation: boolean = false;
   
   constructor() {
     this.playerBoard = [];
@@ -26,20 +35,124 @@ export class BattleshipComponent implements OnInit {
       this.playerBoard.push([]);
       this.enemyBoard.push([]);
       for (let j = 0; j < this.height; j++) {
-        this.playerBoard[i].push([0, 0]);
-        this.enemyBoard[i].push([0, 0]);
+        this.playerBoard[i].push({
+          hasBoat: false,
+          isHit: false,
+          isSelected: false,
+          isValid: false
+        });
+        this.enemyBoard[i].push({
+          hasBoat: false,
+          isHit: false,
+          isSelected: false,
+          isValid: false
+        });
       }
     }
   }
 
   ngOnInit(): void {
   }
+  onRightClick(i: number, j: number) {
+    this.onLeavePlayerSquare(i, j);
+    this.orientation = !this.orientation;
+    this.onEnterPlayerSquare(i, j);
+    return false;
+  }
 
-  onClickPlayerSquare(i: number, j: number) {
-    this.playerBoard[i][j][0] = 1;
+  onClickPlayerSquare(x: number, y: number) {
+    if (this.playerBoard[x][y].isValid) {
+      for (let i = 0; i < this.currentShip; i++) {
+        if (this.orientation) {
+          this.playerBoard[x + i][y].isValid = false;
+          this.playerBoard[x + i][y].hasBoat = true;
+        }
+        else {
+          this.playerBoard[x][y + i].isValid = false;
+          this.playerBoard[x][y + i].hasBoat = true;
+        }
+      }
+      if (this.ships.length > 0) {
+        this.onLeavePlayerSquare(x, y);
+        this.currentShip = this.ships.pop();
+        this.onEnterPlayerSquare(x, y);
+      }
+      else {
+        this.onLeavePlayerSquare(x, y);
+        this.phase = Phase.MainGame;
+      }
+    }
+  }
+  onEnterPlayerSquare(x: number, y: number) {
+    if (this.phase === Phase.Setup) {
+      let valid = true;
+      for (let i = 0; i < this.currentShip; i++) {
+        if (this.orientation) {
+          if (x + i < this.height) {
+            this.playerBoard[x + i][y].isSelected = true;
+            this.playerBoard[x + i][y].isValid = true;
+            if (this.playerBoard[x + i][y].hasBoat) {
+              valid = false;
+            }
+          }
+          else {
+            valid = false;
+          }
+        }
+        else {
+          if (y + i < this.width) {
+            this.playerBoard[x][y + i].isSelected = true;
+            this.playerBoard[x][y + i].isValid = true;
+            if (this.playerBoard[x][y + i].hasBoat) {
+              valid = false;
+            }
+          }
+          else {
+            valid = false;
+          }
+        }
+      }
+
+      if (!valid) {
+        for (let i = 0; i < this.currentShip; i++) {
+          if (this.orientation) {
+            if (x + i < this.height) {
+              this.playerBoard[x + i][y].isValid = false;
+            }
+          }
+          else {
+            if (y + i < this.width) {
+              this.playerBoard[x][y + i].isValid = false;
+            }
+          }
+        }
+      }
+    }
+  }
+  onLeavePlayerSquare(x: number, y: number) {
+    if (this.phase === Phase.Setup) {
+      for(let i = 0; i < this.currentShip; i++) {
+        if (this.orientation) {
+          if (x + i < this.height) {
+            this.playerBoard[x + i][y].isSelected = false;
+            this.playerBoard[x + i][y].isValid = false;
+          }
+        }
+        else {
+          if (y + i < this.height) {
+            this.playerBoard[x][y + i].isSelected = false;
+            this.playerBoard[x][y + i].isValid = false;
+          }
+        }
+      }
+    }
   }
   onClickEnemySquare(i: number, j:number) {
-    this.enemyBoard[i][j][0] = 2;
+    //this.enemyBoard[i][j][0] = 2;
   }
-
+  onClickSinglePlayer() {
+    this.phase = Phase.Setup;
+  }
+  onClickMultiPlayer() {
+  }
 }
