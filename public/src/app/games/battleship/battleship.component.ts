f008be1957e238824e5aef13b6d7804b4f0e6ddf
Hudson <hudsonmpace@gmail.com>
2020-07-23 19:50:42 -0700

    Track entire boats, change color when boat is sunk

--- public/src/app/games/battleship/battleship.component.ts
+++ public/src/app/games/battleship/battleship.component.ts
@@ -14,6 +14,7 @@ type Square = {
   isHit: boolean;
   isSelected: boolean;
   isValid: boolean;
+  isSunk: boolean;
 }
 
 @Component({
@@ -26,12 +27,14 @@ export class BattleshipComponent implements OnInit {
   Mode = Mode;
   playerBoard: Square[][];
   enemyBoard: Square[][];
-  height: number = 20;
-  width: number = 20;
+  height: number = 8;
+  width: number = 8;
   phase: Phase = Phase.Start;
-  ships: number[] = [4, 3, 2];
-  enemyShips: number[] = [4, 3, 2];
-  currentShip: number = this.ships.pop();
+  playerShipLengths: number[] = [4, 3, 2];
+  enemyShipLengths: number[] = [4, 3, 2];
+  playerShips: number[][][] = [];
+  enemyShips: number[][][] = [];
+  currentShip: number = this.playerShipLengths.pop();
   orientation: boolean = false;
   mode: Mode;
   
@@ -46,13 +49,15 @@ export class BattleshipComponent implements OnInit {
           hasBoat: false,
           isHit: false,
           isSelected: false,
-          isValid: false
+          isValid: false,
+          isSunk: false
         });
         this.enemyBoard[i].push({
           hasBoat: false,
           isHit: false,
           isSelected: false,
-          isValid: false
+          isValid: false,
+          isSunk: false
         });
       }
     }
@@ -69,10 +74,10 @@ export class BattleshipComponent implements OnInit {
 
   onClickPlayerSquare(x: number, y: number) {
     if (this.playerBoard[x][y].isValid) {
-      this.placeBoat(this.playerBoard, x, y, this.currentShip, this.orientation);
+      this.placeBoat(this.playerBoard, x, y, this.currentShip, this.orientation, this.playerShips);
       this.onLeavePlayerSquare(x, y);
-      if (this.ships.length > 0) {
-        this.currentShip = this.ships.pop();
+      if (this.playerShipLengths.length > 0) {
+        this.currentShip = this.playerShipLengths.pop();
         this.onEnterPlayerSquare(x, y);
       }
       else {
@@ -92,14 +97,14 @@ export class BattleshipComponent implements OnInit {
   }
   onClickEnemySquare(i: number, j:number) {
     if (this.phase === Phase.MainGame && this.mode === Mode.Singleplayer && !this.enemyBoard[i][j].isHit) {
-      this.enemyBoard[i][j].isHit = true;
+      this.hitSquare(this.enemyBoard, this.enemyShips, i, j);
       let x: number = Math.floor(Math.random() * this.height);
       let y: number = Math.floor(Math.random() * this.width);
       while (this.playerBoard[x][y].isHit) {
         x = Math.floor(Math.random() * this.height);
         y = Math.floor(Math.random() * this.width);
       }
-      this.playerBoard[x][y].isHit = true;
+      this.hitSquare(this.playerBoard, this.playerShips, x, y);
     }
   }
   onClickSinglePlayer() {
@@ -157,23 +162,27 @@ export class BattleshipComponent implements OnInit {
     return valid;
   }
 
-  placeBoat(board: Square[][], x: number, y: number, size: number, orientation: boolean) {
+  placeBoat(board: Square[][], x: number, y: number, size: number, orientation: boolean, boatList: number[][][]) {
+    let boatIndex = boatList.length;
+    boatList.push([]);
     for (let i = 0; i < size; i++) {
       if (orientation) {
         board[x + i][y].isValid = false;
         board[x + i][y].hasBoat = true;
+        boatList[boatIndex].push([x + i, y]);
       }
       else {
         board[x][y + i].isValid = false;
         board[x][y + i].hasBoat = true;
+        boatList[boatIndex].push([x, y + i]);
       }
     }
   }
 
   placeEnemyBoats() {
     let currentShip: number;
-    while (this.enemyShips.length > 0) {
-      currentShip = this.enemyShips.pop();
+    while (this.enemyShipLengths.length > 0) {
+      currentShip = this.enemyShipLengths.pop();
       let x = this.height;
       let y = this.width;
       let orientation = false;
@@ -182,7 +191,41 @@ export class BattleshipComponent implements OnInit {
         y = Math.floor(Math.random() * this.width);
         orientation = Math.random() >= 0.5;
       }
-      this.placeBoat(this.enemyBoard, x, y, currentShip, orientation);
+      this.placeBoat(this.enemyBoard, x, y, currentShip, orientation, this.enemyShips);
+    }
+  }
+
+  hitSquare(board: Square[][], boats: number[][][], x: number, y: number) {
+    board[x][y].isHit = true;
+    let found: boolean = false;
+    if (board[x][y].hasBoat) {
+      for (let i = 0; i < boats.length; i++) {
+        for (let j = 0; j < boats[i].length; j++) {
+          if (boats[i][j][0] === x && boats[i][j][1] === y) {
+            this.tryToSinkBoat(board, boats, i);
+            found = true;
+            break;
+          }
+        }
+        if (found) {
+          break;
+        }
+      }
+    }
+  }
+  tryToSinkBoat(board: Square[][], boats: number[][][], boatIndex: number) {
+    let sunk: boolean = true;
+    for (let i = 0; i < boats[boatIndex].length; i++) {
+      if (!board[boats[boatIndex][i][0]][boats[boatIndex][i][1]].isHit) {
+        sunk = false;
+        break;
+      }
+    }
+    if (sunk) {
+      for (let i = 0; i < boats[boatIndex].length; i++) {
+        board[boats[boatIndex][i][0]][boats[boatIndex][i][1]].isSunk = true;
+      }
+      boats.splice(boatIndex, 1);
     }
   }
 }
